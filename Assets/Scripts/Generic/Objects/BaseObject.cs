12605c460f24df7a64a6d7bb94498ecca3e12205
HDonovan96 <harry.bd11@gmail.com>
2020-02-01 17:10:52 +0000

    Added Object and Weapon scriptable objects

---
+++ Assets/Scripts/Generic/Objects/BaseObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BaseObject : ScriptableObject
+{
+    public enum Rarity
+    {
+        Common,
+        Uncommon,
+        Rare
+    };
+}
---
+++ Assets/Scripts/Generic/Objects/WeaponObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Objects/Create New Weapon")]
+public class WeaponObject : BaseObject
+{
+    public float range = 100.0f;
+    public float damagePerShot = 10.0f;
+    public int magazineSize = 30;
+    public float reloadTime = 1.0f;
+    public float TimeBetweenShots = 1.0f;
+    //public float ScopeTime = 1.0f;
+    // public float zoomMultiplier = 2.0f;
+}
