ca86cd4b7de5a0daab12e12d67a2da6b94d7d40e
HDonovan96 <harry.bd11@gmail.com>
2020-02-01 17:33:27 +0000

    Added hit detection to player

---
+++ Assets/Scripts/Generic/Attack/PlayerAttack.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class PlayerAttack : MonoBehaviourPunCallbacks
+{
+    public WeaponObject equipedWeapon;
+    
+    private Camera charCamera;
+    private float timeSinceLastShot;
+    private float currentBulletsInMag;
+    private float numberOfMags = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        charCamera = this.GetComponentInChildren<Camera>();
+
+        // Variable initialisation.
+        timeSinceLastShot = equipedWeapon.TimeBetweenShots;
+        currentBulletsInMag = equipedWeapon.magazineSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Aborts the script if the GameObject doesn't belong to the client.
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        timeSinceLastShot += Time.deltaTime;
+
+        if (Input.GetButton("Fire1"))
+        {
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        if (CanFire())
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(charCamera.transform.position, charCamera.transform.forward, out hit, equipedWeapon.range))
+            {
+                Debug.LogAssertion(hit.transform.gameObject.name);
+            }
+        }
+    }
+
+    private bool CanFire()
+    {
+        if (timeSinceLastShot > equipedWeapon.TimeBetweenShots)
+        {
+            if (currentBulletsInMag > 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
