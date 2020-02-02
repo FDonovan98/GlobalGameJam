2c53e94365961d1b5fbddfe866a0e90b9cdd3c9c
Ed <edwardjsmithsd@gmail.com>
2020-02-02 12:55:25 +0000

    Object spawning now works. Added folders for health and time prefab
    Object spawner will read in prefabs from weapons, health and time and will randomly select one of those categories and then reandomly spawn an asset from that category per pass

--- Assets/Scripts/Spawn/ObjectSpawn.cs
+++ Assets/Scripts/Spawn/ObjectSpawn.cs
@@ -9,25 +9,28 @@ public class ObjectSpawn : MonoBehaviour
     [SerializeField]
     private float SpawnDiameter;
     private float SpawnRadius;
-    [SerializeField]
-    private float SpawnOffset;
+
     private int MaxItemCount;
     private int CurrentItemCount = 0;
+
+    IDictionary<string, List<string>> ItemDictionary = new Dictionary<string, List<string>>();
+
+    private List<string> WeaponNames;
+    private List<string> HealthPickupNames;
+    private List<string> TimePickupNames;
+
     [SerializeField]
-    private string[] PrefabNames;
-
-    string[] ItemType = { "Weapon", "Health", "Ammo", "Time"};
-
+    private float SpawnOffset;
     public enum Value { Common, Uncommon, Rare}
-
-    private List<string> items = new List<string>();
-
-    public LayerMask Pickup;
 
     [SerializeField]
     private Value Rarity;
 
     Vector3 SquareCentre;
+
+    string AssetPath = null;
+
+    bool SpawnAmmo = false;
     void OnDrawGizmos()
     {
         // Draw a semitransparent blue cube at the transforms position
@@ -38,14 +41,52 @@ public class ObjectSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SquareCentre = transform.position;
-        SpawnRadius = SpawnDiameter / 2;
-        foreach(string s in PrefabNames)
+        switch (Rarity)
         {
-            items.Add(s);
+            case Value.Common:
+                MaxItemCount = 1;
+                break;
+            case Value.Uncommon:
+                MaxItemCount = Random.Range(1, 3);
+                break;
+            case Value.Rare:
+                MaxItemCount = Random.Range(2, 4);
+                break;
+            default:break;
         }
 
+        SquareCentre = transform.position;
+
+        if(SpawnDiameter == 0.0f)
+        {
+            SpawnDiameter = 2.0f;
+        }
+
+        SpawnRadius = SpawnDiameter / 2;
+        
+        WeaponNames = new List<string>();
+        HealthPickupNames = new List<string>();
+        TimePickupNames = new List<string>();
+
+        InitItemLists("Prefabs/Items/Weapons", WeaponNames);
+        InitItemLists("Prefabs/Items/Health", HealthPickupNames);
+        InitItemLists("Prefabs/Items/Time", TimePickupNames);
+
+        ItemDictionary.Add(new KeyValuePair<string, List<string>>("Weapon", WeaponNames));
+        ItemDictionary.Add(new KeyValuePair<string, List<string>>("Health", HealthPickupNames));
+        ItemDictionary.Add(new KeyValuePair<string, List<string>>("Time", TimePickupNames));
+
         SpawnItems();
+    }
+
+    void InitItemLists(string path, List<string> itemlists)
+    {
+        GameObject[] objects = Resources.LoadAll<GameObject>(path);
+
+        foreach (GameObject ob in objects)
+        {
+            itemlists.Add(ob.name);
+        }
     }
 
     void SpawnItems()
@@ -53,15 +94,37 @@ public class ObjectSpawn : MonoBehaviour
         do
         {
             CurrentItemCount++;
-            CreateObject(GetItem(), 0.0f, GetSpawnPosition());
+            CreateObject(GetItem(), GetSpawnPosition());
         } while (CurrentItemCount < MaxItemCount);
     }
 
     string GetItem()
     {
-        int index = Random.Range(0, ItemType.Length);
+        float RandomChance = Random.Range(0.0f, 10.0f);
+        string ItemType = null;
 
-        return items[index];
+        if(RandomChance <= 5.0f)
+        {
+            ItemType = "Weapon";
+            SpawnAmmo = true;
+            AssetPath = "Prefabs/Items/Weapons";
+            CurrentItemCount++;
+        }
+        else if(RandomChance > 5.0f && RandomChance <= 8.5f)
+        {
+            ItemType = "Health";
+            AssetPath = "Prefabs/Items/Health";
+        }
+        else if(RandomChance > 8.5f && RandomChance <= 10.0f)
+        {
+            ItemType = "Time";
+            AssetPath = "Prefabs/Items/Time";
+        }
+       
+
+        int ItemIndex = Random.Range(0, ItemDictionary[ItemType].Count);
+
+        return ItemDictionary[ItemType][ItemIndex];
     }
 
     Vector3 GetSpawnPosition()
@@ -69,14 +132,22 @@ public class ObjectSpawn : MonoBehaviour
         float RandomX = Random.Range(SquareCentre.x - SpawnRadius, SquareCentre.x + SpawnRadius);
         float RandomZ = Random.Range(SquareCentre.z - SpawnRadius, SquareCentre.z + SpawnRadius);
 
-        return new Vector3(RandomX, transform.position.y + SpawnOffset, RandomZ);
+        return new Vector3(RandomX, transform.position.y, RandomZ);
     }
 
-    void CreateObject(string name, float value, Vector3 Pos)
+    void CreateObject(string name, Vector3 Pos)
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            PhotonNetwork.Instantiate(Path.Combine("Prefabs/Networking", name), Pos + (Vector3.up * 2), Quaternion.identity);
+            if(SpawnAmmo)
+            {
+                PhotonNetwork.Instantiate(Path.Combine("Prefabs/Items/Weapons", "Ammo"), Pos + Vector3.right * 2, Quaternion.identity);
+                SpawnAmmo = false;
+            }
+
+            PhotonNetwork.Instantiate(Path.Combine(AssetPath, name), Pos, Quaternion.identity);
         }
+
+        Debug.Log("Spawned: " + name);
     }
 }
